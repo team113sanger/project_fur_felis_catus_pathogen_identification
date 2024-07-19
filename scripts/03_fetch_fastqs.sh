fbc0328a47dcc6b3d593ac77f590fccc65b2c3f5
Jamie Billington <jb63@sanger.ac.uk>
2024-07-19 14:18:26 +0100

    Streamlined upstream

---
+++ scripts/03_fetch_fastqs.sh
@@ -0,0 +1,24 @@
+
+mkdir ${PROJECTDIR}/tmp
+
+#This script takes the cram manifest and generates the SH file with the farm jobs to import and transform to fastq all of the cram files from iRODs
+ 
+
+
+PATH_TO_SEARCH="/lustre/scratch125/casm/team113da/projects/FUR/FUR_analysis/FUR_analysis_cat/pathogen_identification/analysis"
+for manifest in $PATH_TO_SEARCH/*/metadata/*INFO_from_iRODS.txt; do
+    # Extract the STUDY number (first number in the directory name)
+    STUDY=$(basename "$manifest" | cut -d'_' -f1)
+    # Define PROJECTDIR as the full path of the current directory
+    PROJECTDIR=$(dirname $( dirname "$manifest"))
+    echo $PROJECTDIR
+    export TMPDIR=${PROJECTDIR}/tmp
+    # /software/team113/dermatlas/R/R-4.2.2/bin/Rscript \
+    # scripts/dermatlas-starfusion/scripts/cramtofastq_from_iRODs_based_cram_manifest.R \
+    # --manifest $manifest \
+    # --projectdir ${PROJECTDIR} \
+    # --studyID ${STUDY} --mem 16000
+done
+
+
+/bin/sh scripts/
